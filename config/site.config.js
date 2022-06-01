56b5fcc06e399cc29020830a357c7013b038231b
Kurumi <89560821+arakurumi@users.noreply.github.com>
2022-06-01 18:47:39 +0700

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -11,7 +11,7 @@ module.exports = {
 
   // [OPTIONAL] This is the website icon to the left of the title inside the navigation bar. It should be placed under the
   // /public directory of your GitHub project (not your OneDrive folder!), and referenced here by its relative path to /public.
-  icon: 'https://telegra.ph/file/7e7fe8c105bacd551a099.jpg',
+  icon: '/icons.jpg',
 
   // The name of your website. Present alongside your icon.
   title: "Sekai Cloud",
