65baa49f405170b3fa70424f1f505c5f9473bbe4
Kurumi <ayangkurumi@gmail.com>
2022-07-29 13:34:58 +0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,6 +12,8 @@ export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center bg-white dark:bg-gray-900">
       <Head>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6817636686707403"
+     crossorigin="anonymous"></script>
         <title>{siteConfig.title}</title>
       </Head>
 
