4843ebfa563f83560f88917a76270d57807847f0
Kurumi <ayangkurumi@gmail.com>
2022-08-16 14:31:39 +0700

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -38,7 +38,7 @@ module.exports = {
   // [OPTIONAL] The footer component of your website. You can write HTML here, but you need to escape double
   // quotes - changing " to \". You can write anything here, and if you like badges, generate some with https://shields.io
   footer:
-    'Join <a href="https://t.me/arakurumi" target="_blank" rel="noopener noreferrer">Telegram Group</a>',
+    'https://img.shields.io/endpoint?url=https://tg.sumanjay.workers.dev/arakurumi&style=social&logo=telegram',
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
